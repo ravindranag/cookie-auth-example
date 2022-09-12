913925c904bf49c656e18409447bcb686eeb6d30
Ravindra Nag <ravindranag52@gmail.com>
2022-09-12 14:46:16 +0530

    store token in cookies

--- lib/context/SessionProvider.jsx
+++ lib/context/SessionProvider.jsx
@@ -9,13 +9,23 @@ const useSession = () => useContext(SessionContext)
 const SessionProvider = ({ children }) => {
 	const [user, setUser] = useState(null)
 
+	useEffect(() => {
+		const token = Cookies.get('token')
+		if(token) {
+			setUser(Cookies.get('username'))
+		}
+	}, [])
+
 	async function logIn(user) {
 		let success
 		await api.user.login(user)
 			.then(data => {
-				setUser(data)
+				setUser(data.username)
 				success = '1'
 				Cookies.set('username', data.username, {
+					domain: `.${process.env.DOMAIN}`
+				})
+				Cookies.set('token', data.token, {
 					domain: `.${process.env.DOMAIN}`
 				})
 			})
